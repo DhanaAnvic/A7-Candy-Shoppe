a46156b2a54cb4ba1af6da3cfc07ab59cdcf5126
fabed2976 <fabed2976@HRH-IC0027343.wrdsb.ca>
2017-12-13 10:06:59 -0500

    Just add more comments

--- src/Candy.java
+++ src/Candy.java
@@ -6,20 +6,44 @@
 
 /**
  *
- * @author CHANGE_THIS_TO_YOUR_NAME
+ * @author Dhana Anvic
  */
-public class Candy {
+public class Candy extends DessertItem {
+    
+    //create an instance variable 
+    private double weight;
+    private int pricePerLbs;
     
     
-    public Candy(String name, double weight, int pricePerLbs)
-    {
-
+    public Candy(String name, double weight, int pricePerLbs){
+            super(name);
+            this.weight = weight;
+            this.pricePerLbs = pricePerLbs;
+    }
+    
+    
+    @Override
+    public int getCost(){
+        //Multiplying the cost of the candie by the weight and the price per lbs
+        int cost = (int)(Math.round(this.weight) * (this.pricePerLbs));
+        //return the price
+        return cost;
+        
     }
 
- 
-    
-    public String toString()
-    {
+    @Override
+    public String toString(){
+        //create an empty String
+        String output = "";
+        //Converting the price into Dollars and cents
+        String pricetoDollars = DessertShoppe.cents2dollarsAndCents(this.getCost());
+        //Placement for the cost in the receipt
+        int placement = DessertShoppe.RECEIPT_WIDTH - super.getName().length();
+        //the output for the price in the reciept
+        output += this.weight + "lbs. @ $" + DessertShoppe.cents2dollarsAndCents(this.pricePerLbs) + "/ lb. " + super.getName();
+        //retutning the output
+        return output;
+        
        
     }
 }
